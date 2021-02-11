890043975819e80b37c8f41f089a9bbfd86f6caa
Ching-Yi Lee <chingyi8181@gmail.com>
2021-02-12 02:02:41 +0800

    change to detach.numpy

--- a2-distrib/models.py
+++ a2-distrib/models.py
@@ -61,14 +61,25 @@ class NeuralSentimentClassifier(SentimentClassifier):
     Implement your NeuralSentimentClassifier here. This should wrap an instance of the network with learned weights
     along with everything needed to run it on new data (word embeddings, etc.)
     """
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, network, embedding):
+        self.model = network
+        self.model.train(False)
+        self.embedding = embedding
 
     def predict(self, ex_words: List[str]) -> int:
-        pass
+        
+        word_sum = np.zeros(300)
+        for j in range(len(ex_words)):
+            word_embedding = self.embedding.get_embedding(ex_words[j])
+            word_sum += np.array(word_embedding)
+        
+        word_avg = word_sum / len(ex_words)
+        train_x_batch = torch.unsqueeze(torch.from_numpy(word_avg).float(), 0)
+        out = self.model.forward(train_x_batch)[0]
+        return torch.argmax(out)
 
     def predict_all(self, all_ex_words: List[List[str]]) -> List[int]:
-        pass
+        return [self.predict(ex_words) for ex_words in all_ex_words]
 
 def file2embed(sentiment_example, word_embed, max_len=52, word_vec_len=300):
     total_sen = len(sentiment_example)
@@ -87,24 +98,6 @@ def file2embed(sentiment_example, word_embed, max_len=52, word_vec_len=300):
         label = sentiment_example[i].label
         labels.append(label)
     return np.array(res), np.array(labels)
-
-# def file2embed_pad(sentiment_example, word_embed, max_len=52, word_vec_len=50):
-#     total_sen = len(sentiment_example)
-#     res = np.zeros(shape=(total_sen, max_len*50))
-#     labels = []
-#     for i in range(total_sen):
-#         word_ems = []
-#         for j in range(max_len):
-#             if j >= len(sentiment_example[i].words):
-#                 word_ems.append(word_embed.get_embedding("PAD"))
-#             else:
-#                 word_ems.append(word_embed.get_embedding(sentiment_example[i].words[j]))
-#         res[i, :] = np.array(word_ems)
-#         # label = np.zeros(2)
-#         # label[sentiment_example[i].label] = 1
-#         label = sentiment_example[i].label
-#         labels.append(label)
-#     return np.array(res), np.array(labels)
 
     
 def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
@@ -145,22 +138,32 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
             loss = loss_func(outputs, train_y_batch)
             loss.backward()
             output_argmax = torch.argmax(outputs, axis=1)
-            running_acc += sum(output_argmax.numpy() == train_y_batch.numpy()) / len(output_argmax)
+            # running_acc += sum(output_argmax.numpy() == train_y_batch.numpy()) / len(output_argmax)
             optimizer.step()
-            running_loss += loss.item()
+        #     running_loss += loss.item()
         epoch_loss = running_loss/n_iter
-        epoch_acc = running_acc/n_iter
-        print(f"loss: {epoch_loss}")
-        print(f"acc: {epoch_acc}")
+        # epoch_acc = running_acc/n_iter
+        # print(f"loss: {epoch_loss}")
+        # print(f"acc: {epoch_acc}")
         scheduler.step(epoch_loss)
 
-        
+    return NeuralSentimentClassifier(model, word_embeddings)
 
 
-
-
-
-
-
-    # raise NotImplementedError
-
+# def file2embed_pad(sentiment_example, word_embed, max_len=52, word_vec_len=50):
+#     total_sen = len(sentiment_example)
+#     res = np.zeros(shape=(total_sen, max_len*50))
+#     labels = []
+#     for i in range(total_sen):
+#         word_ems = []
+#         for j in range(max_len):
+#             if j >= len(sentiment_example[i].words):
+#                 word_ems.append(word_embed.get_embedding("PAD"))
+#             else:
+#                 word_ems.append(word_embed.get_embedding(sentiment_example[i].words[j]))
+#         res[i, :] = np.array(word_ems)
+#         # label = np.zeros(2)
+#         # label[sentiment_example[i].label] = 1
+#         label = sentiment_example[i].label
+#         labels.append(label)
+#     return np.array(res), np.array(labels)
