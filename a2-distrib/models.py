55ff8daa6fbe6bf79e14f43730be50b0c1f2d66a
Ching-Yi Lee <chingyi8181@gmail.com>
2021-02-12 01:28:40 +0800

    add trainging

--- a2-distrib/models.py
+++ a2-distrib/models.py
@@ -6,6 +6,9 @@ from torch import optim
 import numpy as np
 import random
 from sentiment_data import *
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from torch import optim
 
 
 class SentimentClassifier(object):
@@ -40,6 +43,18 @@ class TrivialSentimentClassifier(SentimentClassifier):
         """
         return 1
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(300, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.fc3 = nn.Linear(50, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+        return x
 
 class NeuralSentimentClassifier(SentimentClassifier):
     """
@@ -48,27 +63,49 @@ class NeuralSentimentClassifier(SentimentClassifier):
     """
     def __init__(self):
         raise NotImplementedError
+
     def predict(self, ex_words: List[str]) -> int:
+        pass
 
     def predict_all(self, all_ex_words: List[List[str]]) -> List[int]:
+        pass
 
-
-def file2embed(sentiment_example, max_len=52, word_vec_len=50):
-#     file = 'data/train.txt'
-#     max_len = 52
+def file2embed(sentiment_example, word_embed, max_len=52, word_vec_len=300):
     total_sen = len(sentiment_example)
-    res = np.zeros(shape=(total_sen, max_len*50))
+    res = []
     labels = []
+    word_sum = np.zeros(word_vec_len)
+
     for i in range(total_sen):
-        word_ems = []
-        for j in range(max_len):
-            if j >= len(sentiment_example[i].words):
-                word_ems.extend(word_embed.get_embedding("PAD"))
-            else:
-                word_ems.extend(word_embed.get_embedding(sentiment_example[i].words[j]))
-        res[i, :] = np.array(word_ems)
-        labels.append(sentiment_example[i].label)
-    return res, np.array(labels)
+        len_sent = len(sentiment_example[i].words)
+        word_sum = np.zeros(word_vec_len)
+        for j in range(len_sent):
+            embedding = word_embed.get_embedding(sentiment_example[i].words[j])
+            word_sum += np.array(embedding)
+        word_avg = word_sum / total_sen
+        res.append(word_avg)
+        label = sentiment_example[i].label
+        labels.append(label)
+    return np.array(res), np.array(labels)
+
+# def file2embed_pad(sentiment_example, word_embed, max_len=52, word_vec_len=50):
+#     total_sen = len(sentiment_example)
+#     res = np.zeros(shape=(total_sen, max_len*50))
+#     labels = []
+#     for i in range(total_sen):
+#         word_ems = []
+#         for j in range(max_len):
+#             if j >= len(sentiment_example[i].words):
+#                 word_ems.append(word_embed.get_embedding("PAD"))
+#             else:
+#                 word_ems.append(word_embed.get_embedding(sentiment_example[i].words[j]))
+#         res[i, :] = np.array(word_ems)
+#         # label = np.zeros(2)
+#         # label[sentiment_example[i].label] = 1
+#         label = sentiment_example[i].label
+#         labels.append(label)
+#     return np.array(res), np.array(labels)
+
     
 def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
     """
@@ -78,10 +115,52 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     :param word_embeddings: set of loaded word embeddings
     :return: A trained NeuralSentimentClassifier model
     """
-    train_x, train_y = 
+    train_x, train_y = file2embed(train_exs, word_embeddings)
+    train_x = train_x*1000
+    val_x, val_y = file2embed(dev_exs, word_embeddings)
+    model = Net()
+    epochs = 100
+    batch_size = 32
+    lr = 0.0001
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, verbose=1, factor=0.5)
+    model.train(True)
+    loss_func = nn.CrossEntropyLoss()
+    for epoch in range(epochs):
+        # train phase
+        n_iter = len(train_x) // batch_size
+        running_loss = 0.0
+        running_acc = 0.0
+        idx = np.linspace(0, len(train_x)-1, len(train_x), dtype=int)
+        random.shuffle(idx)
+        train_x = train_x[idx, ...]
+        train_y = train_y[idx, ...]
+        for i in range(n_iter):
+            train_x_batch = train_x[i:i+batch_size, :]
+            train_y_batch = train_y[i:i+batch_size]
+            train_x_batch = torch.from_numpy(train_x_batch).float() 
+            train_y_batch = torch.from_numpy(train_y_batch)
+            optimizer.zero_grad()
+            outputs = model(train_x_batch)
+            loss = loss_func(outputs, train_y_batch)
+            loss.backward()
+            output_argmax = torch.argmax(outputs, axis=1)
+            running_acc += sum(output_argmax.numpy() == train_y_batch.numpy()) / len(output_argmax)
+            optimizer.step()
+            running_loss += loss.item()
+        epoch_loss = running_loss/n_iter
+        epoch_acc = running_acc/n_iter
+        print(f"loss: {epoch_loss}")
+        print(f"acc: {epoch_acc}")
+        scheduler.step(epoch_loss)
+
+        
 
 
 
 
-    raise NotImplementedError
+
+
+
+    # raise NotImplementedError
 
